580acb7cd82c4e7385038b3ce094a9528702d1da
Eli Barbosa <elirenato2000@gmail.com>
2022-07-06 02:46:40 +0000

    Split services Postgresql and Keycloak

--- dev-services.sh
+++ dev-services.sh
@@ -4,4 +4,31 @@ profile=$1
 
 shift 
 
-docker-compose -f ./docker-compose/$profile/docker-compose.yaml --env-file ./docker-compose/$profile/.env "$@"
+if [[ ! -d "./profiles/$profile" ]];
+then
+   echo "Profile $profile does not exists"
+   exit 1
+fi
+
+if [[ "$profile" == "keycloak-only" && "$1" == *"up"* ]];
+then
+    echo "Checking Postgresql availability"
+    for i in {1..40}
+    do
+        healthStatus=$(docker ps --filter name=postgresql --format='{{json .Status}}')
+        if [[ "$healthStatus" == *"healthy"* ]];
+        then
+            echo -n "Postgresql is available"
+            break
+        elif [[ "$healthStatus" == *"unhealthy"* ]]; 
+    	then
+            echo "Postgresql is NOT available"
+            exit 1
+        fi
+        echo -n "."
+        sleep 3
+    done
+    echo ""
+fi
+
+docker-compose -f ./profiles/$profile/docker-compose.yaml --env-file ./profiles/$profile/.env "$@"
